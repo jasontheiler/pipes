1929464c8d200e3a8a9578522495bbe795959232
Jason Theiler <jasontheiler@pm.me>
2026-01-07 00:38:28 +0100

    feat: add basic enter/leave alternate screen logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::{cursor, terminal};
+
+fn main() -> anyhow::Result<()> {
+    let mut stdout = std::io::stdout();
+
+    terminal::enable_raw_mode()?;
+    crossterm::execute!(stdout, terminal::EnterAlternateScreen, cursor::Hide)?;
+
+    crossterm::event::read()?;
+
+    terminal::disable_raw_mode()?;
+    crossterm::execute!(stdout, terminal::LeaveAlternateScreen, cursor::Show)?;
+
+    Ok(())
 }
