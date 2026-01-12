80febd1c4433b68fa74a9c1a6cbdc2b1a897f4e9
Jason Theiler <jasontheiler@pm.me>
2026-01-12 14:13:56 +0100

    refactor: use strings for arg default values

--- src/args.rs
+++ src/args.rs
@@ -4,20 +4,20 @@ use clap::Parser as _;
 #[command(version, about)]
 pub struct Args {
     /// Number of pipes.
-    #[arg(short, long, default_value_t = 10)]
+    #[arg(short, long, default_value = "8")]
     pub number: u32,
     /// Ticks per second.
-    #[arg(short, long, default_value_t = 60)]
+    #[arg(short, long, default_value = "60")]
     pub tickrate: u32,
     /// Reset after amount of ticks. Set to 0 to never reset.
-    #[arg(short, long, value_name = "TICKS", default_value_t = 600)]
+    #[arg(short, long, value_name = "TICKS", default_value = "600")]
     pub reset: u32,
     /// Probability of a straight pipe fitting.
     #[arg(
         short,
         long = "straight-probability",
         value_name = "PROBABILITY",
-        default_value_t = 0.9
+        default_value = "0.9"
     )]
     pub straight_prob: f64,
 }
